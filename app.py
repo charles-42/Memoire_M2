d55400f1a389b3e7f17318a707043c20f8e700fb
charles-42 <71760090+charles-42@users.noreply.github.com>
2022-03-08 11:41:02 +0100

    update app comments

--- app.py
+++ app.py
@@ -5,7 +5,26 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-st.title('Syllogism resolution')
+#st.title('Syllogism resolution')
+
+#st.write("Natural Language Processing algorithms that aim to perform tasks related to human language are now achieving impressive results in sentiment analysis, translation or text classification. We seek to understand whether they rely on a logic model to perform these tasks. This model could be formal logic, human "logic" (reconstructed from the training corpus) or a specific logic resulting from the algorithms used.")
+
+"""
+# Syllogism resolution 
+
+Natural Language Processing algorithms that aim to perform tasks related to human language are now achieving impressive results in sentiment analysis, translation or text classification. We seek to understand whether they rely on a logic model to perform these tasks. This model could be formal logic, human "logic" (reconstructed from the training corpus) or a specific logic resulting from the algorithms used.
+
+To address this problem, we have tested two pre-trained NLP models against a set of 6500 syllogisms belonging to the 64 classical forms. 
+- The first model is a **`bert-base-uncased`** (named simple in the next dataframes and charts). This model is pretrained on a large unlabelled corpus of English data. We used its next sententence prediction function.  So we gave it as input on one side the two premises of the syllogism and on the other side 8 possible conclusions.For each conclusion the model estimates the probability that it is the next sentence. The conclusion with the highest probability is considered as the answer of the algorithm.
+- The second one is a **`facebook/bart-large-mnli`** (also called mnli). This model is pre-trained on the mnli corpus and aims at predicting whether for a given sentence a second sentence is its entailment, its contradiction or is neutral to it. We gave it as input the two premisses and the 8 possible conclusion. The conclusion with the highest "entailment" probability is considered as the answer.
+
+We also had to study the syllogisms without valid conclusion. It is particularly difficult to make an NLP algorithm predict the absence of valid conclusion. We have explored two ways to tackle this problem. 
+- define a rule such that if the conclusion with the highest probability is below a threshold then the answer is replaced by "Non valid answer".
+- for the mnli algorithm only, if for all conclusion the probability of contradiction or neutral is higher than the probability of entailment then the answer is replaced by "Non valid answer"
+
+You can explore these different scenarios using the slider. For  mnli, the threshold called "max" corresponds to the second path where we compare the probabilities of entailment, contradiction and neutral
+"""
+
 
 
 # Load 10,000 rows of data into the dataframe.
@@ -43,12 +62,16 @@ valid_filtered_data = df_valid[["succes_human",f"succes_simple_{seuil_simple}", 
 valid_mean_filtered = valid_mean[["succes_human",f"succes_simple_{seuil_simple}", f"succes_mnli_{seuil_mnli}"]]
 valid_cor_filtered = valid_cor[[f"succes_simple_{seuil_simple}", f"succes_mnli_{seuil_mnli}"]]
 
+
 if display_graph:
+
+    st.subheader('Chart options')
     sort_way = st.radio(
      "Sort by",
      ('Human success', 'Syllogism Form'))
 
 st.subheader('Valid syllogisms')
+st.write("We first study the syllogisms that have a valid conclusion")
 
 if display_graph:
 
@@ -66,7 +89,7 @@ if display_cor:
     st.write(valid_cor_filtered)
 
 if display_mean:
-    st.caption('Mean')
+    st.caption('Average succes rate by syllogism form')
     st.write(valid_mean_filtered)
 
 if display_raw:
@@ -98,7 +121,7 @@ if display_cor:
     st.write(unvalid_cor_filtered)
 
 if display_mean:
-    st.caption('Mean')
+    st.caption('Average succes rate by syllogism form')
     st.write(unvalid_mean_filtered)
 
 
